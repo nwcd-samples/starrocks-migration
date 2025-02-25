d30e75f7dca07aac1b775b4dd25196f2924e21d1
tingxin <friendship-119@163.com>
2025-02-25 17:03:52 +0000

    update

--- tools/exporter.py
+++ tools/exporter.py
@@ -73,6 +73,7 @@ def run(job_name:str, table_name:str, partition_name = ""):
         for thread in threads:
             thread.join()
 
+    time.sleep(60)
     num_import_threads = int(os.getenv("IMPORT_CONCURRENCY"))
     for i in range(0, num_import_threads):
         send_task_done_notification(job_name)
