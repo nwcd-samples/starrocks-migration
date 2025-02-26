d211c5da73ddcbd0e11ba15eb679d8d1e69159be
tingxin <friendship-119@163.com>
2025-02-26 10:28:35 +0000

    update

--- tools/sparkexporter.py
+++ tools/sparkexporter.py
@@ -97,7 +97,7 @@ def runp(spark: SparkSession, job_name: str, table_name: str, filter_str: str, p
                 "/") else f"{storage}/{job_name}/{db_name}/{table_name}/default/"
 
         
-        logger.info(f"[exporter]begin to {table_name} {pt_name} with {row_count}")
+        logger.info(f"[exporter]begin to {table_name} {pt_name} with")
         starrocksSparkDF.write \
             .option("header", "false") \
             .option("maxRecordsPerFile", max_row_count) \
